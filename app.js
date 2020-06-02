5a2dac01cf3aee43bb0df5de6a37c9d2cb9eaebe
Aleksey <aleksey91scorp@bk.ru>
2020-06-02 22:21:47 +0300

    category added

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ app.listen(3000, function () {
 
 app.get("/", function (req, res) {
   con.query("SELECT * FROM goods", function (error, result) {
-    if (error) {
-      console.error(error);
-      throw err;
-    }
+    if (error) throw err;
     let goods = {};
     for (let i = 0; i < result.length; i++) {
       goods[result[i]["id"]] = result[i];
@@ -31,6 +28,38 @@ app.get("/", function (req, res) {
     console.log(JSON.parse(JSON.stringify(goods)));
     res.render("main", {
       goods: JSON.parse(JSON.stringify(goods)),
+    });
+  });
+});
+
+app.get("/cat", function (req, res) {
+  console.log(req.query.id);
+  let catId = req.query.id;
+
+  let cat = new Promise(function (resolve, reject) {
+    con.query("SELECT * FROM category WHERE id=" + catId, function (
+      error,
+      result
+    ) {
+      if (error) reject(err);
+      resolve(result);
+    });
+  });
+  let goods = new Promise(function (resolve, reject) {
+    con.query("SELECT * FROM goods WHERE category=" + catId, function (
+      error,
+      result
+    ) {
+      if (error) reject(err);
+      resolve(result);
+    });
+  });
+
+  Promise.all([cat, goods]).then(function (value) {
+    console.log(value[0]);
+    res.render("cat", {
+      cat: JSON.parse(JSON.stringify(value[0])),
+      goods: JSON.parse(JSON.stringify(value[1])),
     });
   });
 });
